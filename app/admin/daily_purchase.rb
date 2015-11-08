f6b2cdccabd7bbbae3f4a0f8f4abd58f599334e4
Prelang Builder <builder@prelang.com>
2015-11-08 21:22:14 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'DailyPurchase' to allow for create, update

--- app/admin/daily_purchase.rb
+++ app/admin/daily_purchase.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register DailyPurchase do
+  permit_params :day, :count
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
