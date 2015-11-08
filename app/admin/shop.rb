8a01b3b73d8b8eeb28e5eeaadf7af22762d8de26
Prelang Builder <builder@prelang.com>
2015-11-08 21:22:27 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Shop' to allow for create, update

--- app/admin/shop.rb
+++ app/admin/shop.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Shop do
+  permit_params :url, :shop_id
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
