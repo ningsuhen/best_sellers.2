74e5c022f86b19b902ba964cba16b61f91c1fe39
Prelang Builder <builder@prelang.com>
2015-11-08 21:22:18 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Listing' to allow for create, update

--- app/admin/listing.rb
+++ app/admin/listing.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Listing do
+  permit_params :item_name, :image, :url
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
